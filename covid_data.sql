aa18e579483082ebbf6bb32aac5378ae5726feda
GlobalMid <123549063+GlobalMid@users.noreply.github.com>
2023-11-19 10:12:53 +0500

    covid_data.sql

--- covid_data.sql
+++ covid_data.sql
@@ -165,7 +165,6 @@ where dea.continent is not null
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 where continent is not null 
 --Group By date
 order by 1,2
@@ -200,7 +199,6 @@ order by TotalDeathCount desc
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
@@ -210,6 +208,5 @@ order by PercentPopulationInfected desc
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 Group by Location, Population, date
 order by PercentPopulationInfected desc
